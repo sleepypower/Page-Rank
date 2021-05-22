2ec87ce630f30ed9284e3d2bc611b60fea98725b
David <daalmega@gmail.com>
2021-05-22 17:56:24 -0500

    First commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy
 
 world_wide_web_matrix = numpy.array([
     [0, 0, 1/2, 1/2],
-    [1, 0, 0, 0],
+    [1, 0, 0, 1/2],
     [0, 1/2, 0, 0],
     [0, 1/2, 1/2, 0]
 ])
@@ -18,10 +18,29 @@ def page_rank(world_wide_web_matrix: numpy.array):
 
     number_of_pages = world_wide_web_matrix.shape[0]
 
-    # Create S column vector 
-    S = numpy.array([[1/ number_of_pages for i in range(number_of_pages)]]).T
+    # Create S column vector
+    S = numpy.array([[1 / number_of_pages for i in range(number_of_pages)]]).T
 
+    # Generate eigenvector
+
+    P_before_multiplication = S
+    P = numpy.dot(world_wide_web_matrix, P_before_multiplication)
+    error = numpy.sqrt(numpy.add(
+        P.T, -1 * P_before_multiplication.T).dot(numpy.add(P, -1 * P_before_multiplication)))[0][0]
+    print(error)
+    while abs(error) > 0.00001:
+        P_before_multiplication = P
+        P = numpy.dot(world_wide_web_matrix, P_before_multiplication)
+        error = numpy.sqrt(numpy.add(
+            P.T, -1 * P_before_multiplication.T).dot(numpy.add(P, -1 * P_before_multiplication)))[0][0]
+        print(error)
+
+    return P
 
 
 print(world_wide_web_matrix)
-page_rank(world_wide_web_matrix)
+eigen_vector = page_rank(world_wide_web_matrix)
+print(eigen_vector)
+
+# TODO
+# - Get the the center or centers of the adjacency matrix
