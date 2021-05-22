5eca29e8aaee62ef611e32dae63537dd0460c2a9
David <daalmega@gmail.com>
2021-05-22 15:37:38 -0500

    Added S column vector

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import numpy
 
-world_wide_web = numpy.array([
+world_wide_web_matrix = numpy.array([
     [0, 0, 1/2, 1/2],
     [1, 0, 0, 0],
     [0, 1/2, 0, 0],
     [0, 1/2, 1/2, 0]
 ])
 
-def pageRank():
-    pass
 
-print(world_wide_web)
+def page_rank(world_wide_web_matrix: numpy.array):
+
+    # Ensure the matrix is squared
+    assert world_wide_web_matrix.shape[0] == world_wide_web_matrix.shape[1],\
+        """page_rank Error: The world wide web matrix must be squared,
+        rows: {} cols: {}.""".format(world_wide_web_matrix.shape[0],
+                                     world_wide_web_matrix.shape[1])
+
+    number_of_pages = world_wide_web_matrix.shape[0]
+
+    # Create S column vector 
+    S = numpy.array([[1/ number_of_pages for i in range(number_of_pages)]]).T
+
+
+
+print(world_wide_web_matrix)
+page_rank(world_wide_web_matrix)
