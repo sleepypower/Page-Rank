b6ac90ef8a759f44c2d642191130cd4c42b87abc
David <daalmega@gmail.com>
2021-05-22 15:03:45 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy
+
+world_wide_web = numpy.array([
+    [0, 0, 1/2, 1/2],
+    [1, 0, 0, 0],
+    [0, 1/2, 0, 0],
+    [0, 1/2, 1/2, 0]
+])
+
+def pageRank():
+    pass
+
+print(world_wide_web)
