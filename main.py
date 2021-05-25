5092856df59a55cc851c87c2d4d6630cdb37977d
David <daalmega@gmail.com>
2021-05-25 16:47:57 -0500

    Added documentation

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import numpy
 
-world_wide_web_matrix = numpy.array([
-    [0, 0, 1/2, 1/2],
-    [1, 0, 0, 1/2],
-    [0, 1/2, 0, 0],
-    [0, 1/2, 1/2, 0]
-])
+world_wide_web_matrix = numpy.array([[0, 0, 1, 1/2],
+                                     [1/3, 0, 0, 0],
+                                     [1/3, 1/2, 0, 1/2],
+                                     [1/3, 1/2, 0, 0]])
 
 
 def page_rank(world_wide_web_matrix: numpy.array):
+    """
+    Calculates the rank for every page given an adjacency matrix
+    Uses the formula lim n->Inf (A^n*S) = P where A is the adjacency matrix
+    S is a vector containing 1/#Pages. Finally P is the eigenvector representing
+    the rank of each page.
 
+    Input:
+        world_wide_web_matrix (numpy.array): Adjacency matrix of the world wide web
+    Output:
+        P (numpy.array): Eigenvector representing the rank of each page
+    """
     # Ensure the matrix is squared
     assert world_wide_web_matrix.shape[0] == world_wide_web_matrix.shape[1],\
         """page_rank Error: The world wide web matrix must be squared,
@@ -28,6 +36,7 @@ def page_rank(world_wide_web_matrix: numpy.array):
     error = numpy.sqrt(numpy.add(
         P.T, -1 * P_before_multiplication.T).dot(numpy.add(P, -1 * P_before_multiplication)))[0][0]
     print(error)
+    
     while abs(error) > 0.00001:
         P_before_multiplication = P
         P = numpy.dot(world_wide_web_matrix, P_before_multiplication)
